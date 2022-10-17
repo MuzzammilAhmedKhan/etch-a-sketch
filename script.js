ebc73d5c5f2b8938b0a61aaa84c557f8d1d76c60
MuzzammilAhmedKhan <muzzammilahmedk1@gmail.com>
2022-10-17 20:26:43 +0530

    Generate 256 squares

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+
+var square;
+
+for (let i = 1; i <= 256; i++) {
+  square = document.createElement("div");
+  square.setAttribute("class", "square");
+  square.setAttribute("id", `square-${i}`);
+  square.innerText = `${i}`;
+  container.appendChild(square);
+}
+document.body.appendChild(container);
+
+// console.log(square);
+
